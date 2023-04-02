1d596449063482c23cbd6b1b6fdda492eed2de8c
ShadowCoderVM <sergei.semin2010@yandex.ru>
2023-04-02 12:02:18 +0300

    Adding validation

--- Asparagus/Domain/Models/MessageDto.cs
+++ Asparagus/Domain/Models/MessageDto.cs
@@ -1,8 +1,17 @@
-﻿namespace Asparagus.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asparagus.Domain.Models
 {
-    public record MessageDto(string Name, string Email)
+    public class MessageDto
     {
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указан email")]
+        [EmailAddress(ErrorMessage = "Не корректный email")]
+        [MaxLength(250)]
+        public string Email { get; set; }
 
     }
-   
 }
